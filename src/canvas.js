89ab6ab65a0ed50f7fc5ecf771ad619f18d5a07a
radicalDilettante <wonjun92@gmail.com>
2022-12-13 00:21:34 +1300

    feat: add gaussian weights to canvas

--- src/canvas.js
+++ src/canvas.js
@@ -20,6 +20,9 @@ blur.onclick = () => {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const v1 = performance.now();
   const data = imageData.data;
+  const weights = [0.0545, 0.2442, 0.4026, 0.2442, 0.0545];
+  // const weights = [0.2, 0.2, 0.2, 0.2, 0.2]; // Box
+
   for (let i = 0; i < canvas.height * 4; i = i + 4) {
     for (let j = 0; j < canvas.width * 4; j = j + 4) {
       const redPixel = canvas.width * i + j;
@@ -29,22 +32,23 @@ blur.onclick = () => {
       for (let k = 0; k < 20; k = k + 4) {
         const pixel = j - 8 + k;
         const innerRedPixel = canvas.width * i + pixel;
+        const weight = weights[k / 4];
         if (
           innerRedPixel > 0 &&
           innerRedPixel < canvas.height * canvas.width * 4
         ) {
-          red += data[innerRedPixel];
-          green += data[innerRedPixel + 1];
-          blue += data[innerRedPixel + 2];
+          red += data[innerRedPixel] * weight;
+          green += data[innerRedPixel + 1] * weight;
+          blue += data[innerRedPixel + 2] * weight;
         } else {
-          red += data[redPixel];
-          green += data[redPixel + 1];
-          blue += data[redPixel + 2];
+          red += data[redPixel] * weight;
+          green += data[redPixel + 1] * weight;
+          blue += data[redPixel + 2] * weight;
         }
       }
-      data[redPixel] = red * 0.2;
-      data[redPixel + 1] = green * 0.2;
-      data[redPixel + 2] = blue * 0.2;
+      data[redPixel] = red;
+      data[redPixel + 1] = green;
+      data[redPixel + 2] = blue;
       data[redPixel + 3] = 255;
     }
   }
@@ -58,22 +62,23 @@ blur.onclick = () => {
       for (let k = 0; k < 5 * 4 * canvas.width; k = k + 4 * canvas.width) {
         const pixel = j - 8 * canvas.width + k;
         const innerRedPixel = canvas.width * i + pixel;
+        const weight = weights[k / 4 / canvas.width];
         if (
           innerRedPixel > 0 &&
           innerRedPixel < canvas.height * 4 * canvas.width
         ) {
-          red += data[innerRedPixel];
-          green += data[innerRedPixel + 1];
-          blue += data[innerRedPixel + 2];
+          red += data[innerRedPixel] * weight;
+          green += data[innerRedPixel + 1] * weight;
+          blue += data[innerRedPixel + 2] * weight;
         } else {
-          red += data[redPixel];
-          green += data[redPixel + 1];
-          blue += data[redPixel + 2];
+          red += data[redPixel] * weight;
+          green += data[redPixel + 1] * weight;
+          blue += data[redPixel + 2] * weight;
         }
       }
-      data[redPixel] = red * 0.2;
-      data[redPixel + 1] = green * 0.2;
-      data[redPixel + 2] = blue * 0.2;
+      data[redPixel] = red;
+      data[redPixel + 1] = green;
+      data[redPixel + 2] = blue;
       data[redPixel + 3] = 255;
     }
   }
