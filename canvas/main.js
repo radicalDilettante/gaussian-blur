9ac54845fda29583d78ebbedf8a6e674bbe5ec73
radicalDilettante <wonjun92@gmail.com>
2022-12-12 15:10:20 +1300

    feat: canvas

--- canvas/main.js
+++ canvas/main.js
@@ -3,11 +3,13 @@ let imageData, ctx;
 const image = new Image();
 image.src = "../auckland.jpg";
 image.onload = () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style.height = window.innerHeight * 0.9 + "px";
+  const width = 800;
+  const height = 600;
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.height = height * 0.9 + "px";
   ctx = canvas.getContext("2d");
-  ctx.drawImage(image, 0, 0);
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
   imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
@@ -19,24 +21,67 @@ blur.onclick = () => {
   const data = imageData.data;
   for (let i = 0; i < canvas.height * 4; i = i + 4) {
     for (let j = 0; j < canvas.width * 4; j = j + 4) {
-      const r = canvas.width * i + j;
-      data[r] = 255;
-      data[r + 1] = 255;
-      data[r + 2] = 255;
-      data[r + 3] = 255;
+      const redPixel = canvas.width * i + j;
+      let red = 0;
+      let green = 0;
+      let blue = 0;
+      for (let k = 0; k < 20; k = k + 4) {
+        const pixel = j - 8 + k;
+        const innerRedPixel = canvas.width * i + pixel;
+        if (
+          innerRedPixel > 0 &&
+          innerRedPixel < canvas.height * canvas.width * 4
+        ) {
+          red += data[innerRedPixel];
+          green += data[innerRedPixel + 1];
+          blue += data[innerRedPixel + 2];
+        } else {
+          red += data[redPixel];
+          green += data[redPixel + 1];
+          blue += data[redPixel + 2];
+        }
+      }
+      data[redPixel] = red * 0.2;
+      data[redPixel + 1] = green * 0.2;
+      data[redPixel + 2] = blue * 0.2;
+      data[redPixel + 3] = 255;
     }
   }
-  //   for (let i = 0; i < data.byteLength; i = i + 4) {
-  //     data[i] = 255;
-  //     data[i + 1] = 255;
-  //     data[i + 2] = 255;
-  //     data[i + 3] = 255;
-  //   }
+
+  for (let i = 0; i < canvas.height * 4; i = i + 4) {
+    for (let j = 0; j < canvas.width * 4; j = j + 4) {
+      const redPixel = canvas.width * i + j;
+      let red = 0;
+      let green = 0;
+      let blue = 0;
+      for (let k = 0; k < 5 * 4 * canvas.width; k = k + 4 * canvas.width) {
+        const pixel = j - 8 * canvas.width + k;
+        const innerRedPixel = canvas.width * i + pixel;
+        if (
+          innerRedPixel > 0 &&
+          innerRedPixel < canvas.height * 4 * canvas.width
+        ) {
+          red += data[innerRedPixel];
+          green += data[innerRedPixel + 1];
+          blue += data[innerRedPixel + 2];
+        } else {
+          red += data[redPixel];
+          green += data[redPixel + 1];
+          blue += data[redPixel + 2];
+        }
+      }
+      data[redPixel] = red * 0.2;
+      data[redPixel + 1] = green * 0.2;
+      data[redPixel + 2] = blue * 0.2;
+      data[redPixel + 3] = 255;
+    }
+  }
+
   ctx.putImageData(imageData, 0, 0);
   const v2 = performance.now();
   console.log(v2 - v1);
 };
 
 reset.onclick = () => {
-  ctx.drawImage(image, 0, 0);
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 };
